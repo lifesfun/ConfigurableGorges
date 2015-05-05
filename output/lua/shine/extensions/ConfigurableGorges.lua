b699c1e1e9a61ef53b9c04e5a59832653c93167f
lifesfun <dejong.nk@gmail.com>
2015-05-05 14:09:06 -0700

    config updates

--- output/lua/shine/extensions/ConfigurableGorges.lua
+++ output/lua/shine/extensions/ConfigurableGorges.lua
@@ -1,9 +1,15 @@
+--[[ 
+--Created by lifesfun http://steamcommunity.com/id/lifesfun
+--]]
 local Shine = Shine
 
 local Plugin = {} 
 
 Plugin.Version = "0.1"
 Plugin.HasConfig = true
+Plugin.CheckConfig = true
+Plugin.DefaultState = true
+Plugin.Ns2Only = false
 Plugin.ConfigName = "ConfigurableGorges.json"
 
 Plugin.DefaultConfig = {
@@ -32,3 +38,5 @@ function Plugin:Cleanup()
 	self.BaseClass.Cleanup()
 	self.Enabled = false
 end
+
+Shine:RegisterExtension( "configurablegorges" , Plugin )
